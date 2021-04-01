695b502570cb5294d16866e1fcf56c33cf0d763a
Munatsi Matipano <40175171+Apoth3os1s@users.noreply.github.com>
2021-04-01 12:00:57 +1100

    Add files via upload

---
+++ BlinkName.ino
@@ -0,0 +1,109 @@
+int led1 = D6;
+int led2 = D7;
+
+void setup() {
+
+	pinMode(led1, OUTPUT);
+	pinMode(led2, OUTPUT);
+
+}
+void loop() {
+	//M
+	delay(2000);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(1000);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	delay(500);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(1000);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	//U
+	delay(1000);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(500);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	delay(500);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(500);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	delay(500);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(1000);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	//N
+	delay(1000);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(1000);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	delay(500);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(500);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	//A
+	delay(1000);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(500);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	delay(500);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(1000);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	//T
+	delay(1000);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(1000);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	//S
+	delay(1000);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(500);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	delay(500);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(500);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	delay(500);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(500);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	//I
+	delay(1000);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(500);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	delay(500);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	delay(500);
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	delay(1000);
+}
